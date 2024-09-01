b6ee5be254f226739a7fff574c76bae78001723e
Suebin Park <90715726+suebinpark@users.noreply.github.com>
2024-09-01 23:43:42 +0900

    mobile detection

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,3 +1,7 @@
+if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+  alert("모바일 기기에서 접속 중입니다. 효과적인 '물음표 괴물' 팝업 환경을 위해서 PC 브라우저로 접속해주시기 바랍니다.");
+}
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
